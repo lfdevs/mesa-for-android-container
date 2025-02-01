028be5b318c8d4c80f76381fae03fd4e15ccc4a5
xMeM <haooy@outlook.com>
2025-02-02 04:26:45 +0800

    wrapper: Optimize build speed and target file size

--- src/vulkan/wrapper/wrapper_device_memory.c
+++ src/vulkan/wrapper/wrapper_device_memory.c
@@ -1,10 +1,6 @@
-#define native_handle_t __native_handle_t
-#define buffer_handle_t __buffer_handle_t
 #include "wrapper_private.h"
 #include "wrapper_entrypoints.h"
 #include "vk_common_entrypoints.h"
-#undef native_handle_t
-#undef buffer_handle_t
 #include "util/os_file.h"
 #include "vk_util.h"
 
