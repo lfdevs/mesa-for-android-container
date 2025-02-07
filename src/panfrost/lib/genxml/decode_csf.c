d507d75a16281d349f634779c267b06eec8606c0
Timo Aaltonen <tjaalton@debian.org>
2025-02-07 19:30:14 +0200

    25.0.0~rc2-1 (patches unapplied)
    Imported using git-ubuntu import.

--- src/panfrost/lib/genxml/decode_csf.c
+++ src/panfrost/lib/genxml/decode_csf.c
@@ -595,7 +595,7 @@ pandecode_run_tiling(struct pandecode_context *ctx, FILE *fp,
                  cs_get_u64(qctx, 48));
 
    uint64_t blend = cs_get_u64(qctx, 50);
-   GENX(pandecode_blend_descs)(ctx, blend & ~7, blend & 7, 0, qctx->gpu_id);
+   GENX(pandecode_blend_descs)(ctx, blend & ~15, blend & 15, 0, qctx->gpu_id);
 
    DUMP_ADDR(ctx, DEPTH_STENCIL, cs_get_u64(qctx, 52), "Depth/stencil");
 
@@ -610,6 +610,7 @@ pandecode_run_tiling(struct pandecode_context *ctx, FILE *fp,
 
    ctx->indent--;
 }
+
 static void
 pandecode_run_idvs(struct pandecode_context *ctx, FILE *fp,
                    struct queue_ctx *qctx, struct MALI_CS_RUN_IDVS *I)
@@ -726,7 +727,7 @@ pandecode_run_idvs(struct pandecode_context *ctx, FILE *fp,
       pandecode_log(ctx, "Varying allocation: %u\n", cs_get_u32(qctx, 48));
 
    uint64_t blend = cs_get_u64(qctx, 50);
-   GENX(pandecode_blend_descs)(ctx, blend & ~7, blend & 7, 0, qctx->gpu_id);
+   GENX(pandecode_blend_descs)(ctx, blend & ~15, blend & 15, 0, qctx->gpu_id);
 
    DUMP_ADDR(ctx, DEPTH_STENCIL, cs_get_u64(qctx, 52), "Depth/stencil");
 
@@ -1145,7 +1146,7 @@ record_indirect_branch_target(struct cs_code_cfg *cfg,
 {
    union {
       uint32_t u32[256];
-      uint32_t u64[256];
+      uint64_t u64[128];
    } reg_file = {0};
 
    list_add(&cur_blk->node, blk_stack);
@@ -1158,7 +1159,11 @@ record_indirect_branch_target(struct cs_code_cfg *cfg,
          switch (base.opcode) {
          case MALI_CS_OPCODE_MOVE: {
             cs_unpack(instr, CS_MOVE, I);
-            reg_file.u64[I.destination] = I.immediate;
+
+            assert(I.destination % 2 == 0 &&
+                   "Destination register should be aligned to 2");
+
+            reg_file.u64[I.destination / 2] = I.immediate;
             break;
          }
 
@@ -1176,7 +1181,14 @@ record_indirect_branch_target(struct cs_code_cfg *cfg,
 
          case MALI_CS_OPCODE_ADD_IMMEDIATE64: {
             cs_unpack(instr, CS_ADD_IMMEDIATE64, I);
-            reg_file.u64[I.destination] = reg_file.u64[I.source] + I.immediate;
+
+            assert(I.destination % 2 == 0 &&
+                   "Destination register should be aligned to 2");
+            assert(I.source % 2 == 0 &&
+                   "Source register should be aligned to 2");
+
+            reg_file.u64[I.destination / 2] =
+               reg_file.u64[I.source / 2] + I.immediate;
             break;
          }
 
@@ -1198,8 +1210,10 @@ record_indirect_branch_target(struct cs_code_cfg *cfg,
    uint64_t *instr = &cfg->instrs[ibranch->instr_idx];
    cs_unpack(instr, CS_JUMP, I);
 
+   assert(I.address % 2 == 0 && "Address register should be aligned to 2");
+
    struct cs_indirect_branch_target target = {
-      .address = reg_file.u64[I.address],
+      .address = reg_file.u64[I.address / 2],
       .length = reg_file.u32[I.length],
    };
 
--- src/panfrost/vulkan/csf/panvk_vX_cmd_draw.c
+++ src/panfrost/vulkan/csf/panvk_vX_cmd_draw.c
@@ -1526,7 +1526,12 @@ prepare_dcd(struct panvk_cmd_buffer *cmdbuf)
 
             cfg.pixel_kill_operation = earlyzs.kill;
             cfg.zs_update_operation = earlyzs.update;
-            cfg.evaluate_per_sample = fs->info.fs.sample_shading;
+            cfg.evaluate_per_sample = fs->info.fs.sample_shading &&
+                                      (dyns->ms.rasterization_samples > 1);
+
+            cfg.shader_modifies_coverage = fs->info.fs.writes_coverage ||
+                                           fs->info.fs.can_discard ||
+                                           alpha_to_coverage;
          } else {
             cfg.allow_forward_pixel_to_kill = true;
             cfg.allow_forward_pixel_to_be_killed = true;
@@ -2076,6 +2081,7 @@ panvk_per_arch(cmd_inherit_render_state)(
       to_panvk_physical_device(dev->vk.physical);
    struct pan_fb_info *fbinfo = &cmdbuf->state.gfx.render.fb.info;
 
+   cmdbuf->state.gfx.render.suspended = false;
    cmdbuf->state.gfx.render.flags = inheritance_info->flags;
 
    gfx_state_set_dirty(cmdbuf, RENDER_STATE);
@@ -2609,6 +2615,7 @@ panvk_per_arch(CmdEndRendering)(VkCommandBuffer commandBuffer)
     * so any barrier encountered after EndRendering() doesn't try to flush
     * draws. */
    cmdbuf->state.gfx.render.flags = 0;
+   cmdbuf->state.gfx.render.suspended = suspending;
 
    /* If we're not suspending, we need to resolve attachments. */
    if (!suspending)
