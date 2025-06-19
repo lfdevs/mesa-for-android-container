JMarcosHP/mesa-custom

commit ac886e3f55bb46584723b452a6ca661f3feb668d

Obsolete package libglapi
In mesa 25.0 libglapi.*.so (shared-glapi) is merged into libgallium.*.so
and exported from it for libGL and libEGL:
https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32789

Obsolete the libglapi package. Since libgallium.*.so is provided by
mesa-dri-drivers, add the obsoletes there.

mesa.spec
@@ -231,6 +231,7 @@ Requires:       %{name}-filesystem%{?_isa} = %{?epoch:%{epoch}:}%{version}-%{rel
 %if 0%{?with_va}
 Recommends:     %{name}-va-drivers%{?_isa}
 %endif
+Obsoletes:      %{name}-libglapi < 25.0.0~rc2-1
 
 %description dri-drivers
 %{summary}.

lfdevs/mesa-for-android-container

commit 8c90bf590e5e11aae792023f3c91efe66d9c3c1a

Add missing provides for libglapi
Found by CentOS CI.

Fixes: ac886e3f55bb ("ac886e3f55bb46584723b452a6ca661f3feb668d")

mesa.spec
@@ -245,6 +245,7 @@ Requires:       %{name}-filesystem%{?_isa} = %{?epoch:%{epoch}:}%{version}-%{rel
 Recommends:     %{name}-va-drivers%{?_isa}
 %endif
 Obsoletes:      %{name}-libglapi < 25.0.0~rc2-1
+Provides:       %{name}-libglapi >= 25.0.0~rc2-1
 
 %description dri-drivers
 %{summary}.