06f4b730277bbed83080e29943258588db0e37b1
lfdevs <109842948+lfdevs@users.noreply.github.com>
2025-12-11 22:40:23 +0800

    Fix Adreno 725/730
    The patch is from @Weab-chan: https://github.com/Weab-chan/freedreno_turnip-CI/commit/95b9fe54607f22cddfde82dfaf6d8f1158611f81

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -878,6 +878,7 @@ a7xx_gen1 = A7XXProps(
         fs_must_have_non_zero_constlen_quirk = True,
         enable_tp_ubwc_flag_hint = True,
         reading_shading_rate_requires_smask_quirk = True,
+        compute_constlen_quirk = True,
     )
 
 a7xx_gen2 = A7XXProps(
