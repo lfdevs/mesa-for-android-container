41ab755d33b318e6d1db16888f298042b8a7f68d
whitebelyash <whbexiumwork@gmail.com>
2026-01-16 22:55:20 +0400

    tu/freedreno: add disable_gmem GPU property
    Disables GMEM usage completely, effectively same thing as TU_DEBUG=sysmem
    
    Signed-off-by: whitebelyash <whbexiumwork@gmail.com>

--- src/freedreno/common/freedreno_dev_info.h
+++ src/freedreno/common/freedreno_dev_info.h
@@ -474,20 +474,18 @@ struct fd_dev_info {
        * expected:
        */
       bool has_salu_int_narrowing_quirk;
-
       /* Whether the device supports the image processing opcode */
       bool has_image_processing;
-
       /* The amount of valid draw state IDs. */
       uint32_t max_draw_states;
-
       /* Whether GRAS_CL_INTERP_CNTL has FACENESS/CENTERRHW and thus
        * being able to avoid setting ij_linear_sample for FragFace/FragCoord.
        */
       bool has_implicit_fragface_fragcoord_ij_linear;
-
       uint32_t max_texel_buffer_range_elements;
       uint32_t max_storage_buffer_range_bytes;
+      /* If GMEM needs to be disabled for this GPU */
+      bool disable_gmem;
    } props;
 };
 
