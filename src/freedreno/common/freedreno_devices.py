409cc471f2a3d09fb5676a3a0aed58a0a685567d
lfdevs <109842948+lfdevs@users.noreply.github.com>
2026-04-23 11:49:15 +0800

    freedreno: remove unofficial support for Adreno 810
    This partly reverts commit 54a3dce393b708dea6422b77d3912abda2ea94dd
    Remove unofficial support for Adreno 810, as upstream has added official support: https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/40613

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1383,48 +1383,6 @@ a8xx_825 = GPUProps(
         gmem_per_ccu_depth_cache_size = 128 * 1024,
 )
 
-a8xx_810 = GPUProps(
-        sysmem_vpc_attr_buf_size = 131072,
-        sysmem_vpc_pos_buf_size = 65536,
-        sysmem_vpc_bv_pos_buf_size = 32768,
-        # These values are maximum size of depth/color cache for current A8XX Gen2 sysmem configuration
-        # Bigger values cause an integer underflow in freedreno gmem calculations
-        sysmem_ccu_color_cache_fraction = CCUColorCacheFraction.FULL.value,
-        sysmem_per_ccu_color_cache_size = 32 * 1024,
-        sysmem_ccu_depth_cache_fraction = CCUColorCacheFraction.THREE_QUARTER.value,
-        sysmem_per_ccu_depth_cache_size = 32 * 1024,
-        gmem_vpc_attr_buf_size = 49152,
-        gmem_vpc_pos_buf_size = 24576,
-        gmem_vpc_bv_pos_buf_size = 32768,
-        gmem_ccu_color_cache_fraction = CCUColorCacheFraction.EIGHTH.value,
-        gmem_per_ccu_color_cache_size = 16 * 1024,
-        gmem_ccu_depth_cache_fraction = CCUColorCacheFraction.FULL.value,
-        gmem_per_ccu_depth_cache_size = 64 * 1024,
-        # FD810 does not support ray tracing
-        has_ray_intersection = False,
-        has_sw_fuse = False, # ????
-)
-
-# gen8_3_0
-add_gpus([
-        GPUId(chip_id=0x44010000, name="FD810"),
-    ], A6xxGPUInfo(
-        CHIP.A8XX,
-        [a7xx_base, a7xx_gen3, a8xx_base, a8xx_810],
-        num_ccu = 2,
-        num_slices = 1,
-        tile_align_w = 96,
-        tile_align_h = 32,
-        tile_max_w = 16416,
-        tile_max_h = 16384,
-        num_vsc_pipes = 32,
-        cs_shared_mem_size = 32 * 1024,
-        wave_granularity = 2,
-        fibers_per_sp = 128 * 2 * 16,
-        magic_regs = dict(),
-        raw_magic_regs = a8xx_base_raw_magic_regs,
-    ))
-
 # gen8_6_0
 add_gpus([
         GPUId(chip_id=0x44030000, name="FD825"),
