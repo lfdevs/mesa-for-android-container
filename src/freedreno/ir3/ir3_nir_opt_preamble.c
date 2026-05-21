41aa0733339942f639c90b566a4723b60e1f06ce
Connor Abbott <cwabbott0@gmail.com>
2026-05-21 12:10:41 -0400

    ir3/opt_preamble: Consider speculatability correctly
    Now that producers set it, stop setting it on everything. Base
    descriptor speculatability on the information in ir3_bindless_resource
    now that it is plumbed through.

--- src/freedreno/ir3/ir3_nir_opt_preamble.c
+++ src/freedreno/ir3/ir3_nir_opt_preamble.c
@@ -268,44 +268,6 @@ avoid_instr(const nir_instr *instr, const void *data)
    return intrin->intrinsic == nir_intrinsic_bindless_resource_ir3;
 }
 
-static bool
-set_speculate(nir_builder *b, nir_instr *instr, UNUSED void *_)
-{
-   if (instr->type == nir_instr_type_tex) {
-      nir_instr_as_tex(instr)->can_speculate = true;
-      return true;
-   }
-
-   if (instr->type != nir_instr_type_intrinsic)
-      return false;
-
-   nir_intrinsic_instr *intr = nir_instr_as_intrinsic(instr);
-
-   switch (intr->intrinsic) {
-   /* These instructions go through bounds-checked hardware descriptors so
-    * should be safe to speculate.
-    *
-    * TODO: This isn't necessarily true in Vulkan, where descriptors don't need
-    * to be filled out and bindless descriptor offsets aren't bounds checked.
-    * We may need to plumb this information through from turnip for correctness
-    * to avoid regressing freedreno codegen.
-    */
-   case nir_intrinsic_load_ubo:
-   case nir_intrinsic_load_ubo_vec4:
-   case nir_intrinsic_image_load:
-   case nir_intrinsic_image_samples_identical:
-   case nir_intrinsic_bindless_image_load:
-   case nir_intrinsic_load_ssbo:
-   case nir_intrinsic_load_ssbo_ir3:
-      nir_intrinsic_set_access(intr, nir_intrinsic_access(intr) |
-                                     ACCESS_CAN_SPECULATE);
-      return true;
-
-   default:
-      return false;
-   }
-}
-
 bool
 ir3_nir_opt_preamble(nir_shader *nir, struct ir3_shader_variant *v)
 {
@@ -323,9 +285,6 @@ ir3_nir_opt_preamble(nir_shader *nir, struct ir3_shader_variant *v)
    if (max_size == 0)
       return false;
 
-   bool progress = nir_shader_instructions_pass(nir, set_speculate,
-                                                nir_metadata_control_flow, NULL);
-
    nir_opt_preamble_options options = {
       .drawid_uniform = true,
       .subgroup_size_uniform = true,
@@ -339,7 +298,7 @@ ir3_nir_opt_preamble(nir_shader *nir, struct ir3_shader_variant *v)
    };
 
    unsigned size = 0;
-   progress |= nir_opt_preamble(nir, &options, &size);
+   bool progress = nir_opt_preamble(nir, &options, &size);
 
    if (!v->binning_pass) {
       uint32_t preamble_size_vec4 =
@@ -596,6 +555,33 @@ get_descriptors(nir_instr *instr, nir_def **descs)
    }
 }
 
+static bool
+is_descriptor_prefetch_speculatable(nir_def *desc)
+{
+   nir_instr *instr = nir_def_instr(desc);
+
+   /* Non-bindless descriptors are always speculatable */
+   if (instr->type != nir_instr_type_intrinsic)
+      return true;
+
+   nir_intrinsic_instr *bindless = nir_instr_as_intrinsic(nir_def_instr(desc));
+
+   if (bindless->intrinsic != nir_intrinsic_bindless_resource_ir3)
+      return true;
+
+   return nir_intrinsic_access(bindless) & ACCESS_CAN_SPECULATE;
+}
+
+static bool
+is_descriptor_prefetchable(nir_def *desc)
+{
+   nir_instr *instr = nir_def_instr(desc);
+
+   return instr->block->cf_node.parent->type == nir_cf_node_function ||
+      is_descriptor_prefetch_speculatable(desc);
+}
+
+
 #define MAX_PREFETCHES 32
 
 struct prefetches {
@@ -627,9 +613,11 @@ struct prefetch_state {
 
 static bool
 emit_descriptor_prefetch(nir_builder *b, nir_instr *instr, nir_def **descs,
-                         struct prefetch_state *state)
+                         struct prefetch_state *state, bool can_speculate)
 {
    nir_block *insert_block = nir_def_block(descs[0]);
+
+   enum gl_access_qualifier access = can_speculate ? ACCESS_CAN_SPECULATE : 0;
 
    if (descs[1]) {
       insert_block = find_insert_block_for_defs(descs, 2);
@@ -679,13 +667,13 @@ emit_descriptor_prefetch(nir_builder *b, nir_instr *instr, nir_def **descs,
          if (!sampler_already_prefetched)
             add_prefetch(&state->sampler, descs[1]);
 
-         nir_prefetch_sam_ir3(b, descs[0], descs[1]);
+         nir_prefetch_sam_ir3(b, descs[0], descs[1], .access = access);
       } else {
          if (tex_already_prefetched)
             return false;
 
          add_prefetch(&state->tex, descs[0]);
-         nir_prefetch_tex_ir3(b, descs[0]);
+         nir_prefetch_tex_ir3(b, descs[0], .access = access);
       }
    } else {
       assert(instr->type == nir_instr_type_intrinsic);
@@ -700,9 +688,9 @@ emit_descriptor_prefetch(nir_builder *b, nir_instr *instr, nir_def **descs,
 
       nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
       if (intrin->intrinsic == nir_intrinsic_load_ubo)
-         nir_prefetch_ubo_ir3(b, descs[0]);
+         nir_prefetch_ubo_ir3(b, descs[0], .access = access);
       else
-         nir_prefetch_tex_ir3(b, descs[0]);
+         nir_prefetch_tex_ir3(b, descs[0], .access = access);
    }
 
    return true;
@@ -776,28 +764,19 @@ ir3_nir_opt_prefetch_descriptors(nir_shader *nir, struct ir3_shader_variant *v)
                should_prefetch_descriptor(descs[1])))
             continue;
 
-         /* The instruction itself must be hoistable.
-          * TODO: If the descriptor is statically referenced and in-bounds, then
-          * we should be able to hoist the descriptor load even if the
-          * descriptor contents aren't guaranteed. This would require more
-          * plumbing.
-          * TODO: Textures. This is broken in nir_opt_preamble at the moment and
-          * handling them would also require more plumbing.
-          */
-         if (instr->type == nir_instr_type_intrinsic &&
-             nir_intrinsic_has_access(nir_instr_as_intrinsic(instr)) &&
-             !(nir_intrinsic_access(nir_instr_as_intrinsic(instr)) &
-               ACCESS_CAN_SPECULATE) &&
-             block->cf_node.parent->type != nir_cf_node_function)
-            continue;
-
-         /* Each descriptor must be rematerializable */
+         /* Each descriptor must be rematerializable and speculatable */
          if (descs[0] &&
-             !ir3_def_is_rematerializable_for_preamble(descs[0], preamble_defs))
+             (!is_descriptor_prefetchable(descs[0]) ||
+              !ir3_def_is_rematerializable_for_preamble(descs[0], preamble_defs)))
             continue;
          if (descs[1] &&
-             !ir3_def_is_rematerializable_for_preamble(descs[1], preamble_defs))
+             (!is_descriptor_prefetchable(descs[1]) ||
+              !ir3_def_is_rematerializable_for_preamble(descs[1], preamble_defs)))
             continue;
+
+         bool is_speculatable =
+            (!descs[0] || is_descriptor_prefetch_speculatable(descs[0])) &&
+            (!descs[1] || is_descriptor_prefetch_speculatable(descs[1]));
 
          /* If the preamble hasn't been created then this descriptor isn't a
           * duplicate and we will definitely insert an instruction, so create
@@ -849,7 +828,8 @@ ir3_nir_opt_prefetch_descriptors(nir_shader *nir, struct ir3_shader_variant *v)
                                                   preamble_defs);
          }
 
-         progress |= emit_descriptor_prefetch(&b, instr, preamble_descs, &state);
+         progress |= emit_descriptor_prefetch(&b, instr, preamble_descs, &state,
+                                              is_speculatable);
 
          if (state.sampler.num_prefetches == MAX_PREFETCHES &&
              state.tex.num_prefetches == MAX_PREFETCHES)
